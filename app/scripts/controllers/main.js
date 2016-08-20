3a0add583c634a0c02323ff87e5ae1ad61a52152
Alexander Murphy <amurphy345@gmail.com>
2016-08-19 21:27:43 -0600

    working on cart

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,15 @@ angular.module('App')
   .controller('MainCtrl', function (menu, $http, $scope) {
     // $http.get('/scripts/mean_tea_data.json').then(res => $scope.menu = res)
     console.log(menu)
+    $scope.cart = []
     $scope.menu = menu.data
+    $scope.addToCart = (id) => {
+      var menuItem = menu.data.filter(i => i._id === id)[0]
+      $scope.cart.push(menuItem)
+      console.log(menuItem)
+    }
+    $scope.goToCart = () => {
+      
+    }
    
   });
